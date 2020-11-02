ca98c69a76f40548ecb329a682e8992c7b7ad93f
Mahmudjon Rahimov <mrahimov72@gmail.com>
2020-11-02 23:52:51 +0300

    Initial commit

---
+++ src/main/java/ru/bmstu/iu3/beautyroomzlapp/BeautyRoomZlAppApplication.java
@@ -0,0 +1,13 @@
+package ru.bmstu.iu3.beautyroomzlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyRoomZlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautyRoomZlAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/bmstu/iu3/beautyroomzlapp/BeautyRoomZlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bmstu.iu3.beautyroomzlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautyRoomZlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
